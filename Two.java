d9a8cba359bc20b6902e4a6f9b0c3e3201299845
FemkeSpaans <55233583+FemkeSpaans@users.noreply.github.com>
2020-09-11 12:57:22 +0200

    Update Two.java

--- Two.java
+++ Two.java
@@ -16,6 +16,17 @@ public class Two {
         Point point1 = new Point(1, 1);
         Point point2 = point1;
         
+        // In a reference type, it references to the object in memory, they do not store the actual values,
+        // But a reference to said value. In this cause point1 and point2 reference the same object in memory.
+        // These two variables are not independent of each other. Because they both reference the same object,
+        // If one changes, so does the other.
+
+        // Using the first reference value to update both
+        point1.x = 2;
+        System.out.println(point2);
+        // Reference types are copied by the references, whereas primitive types are copied by their value,
+        // and these values are completely independent of each other. 
+        
 
 
     }
