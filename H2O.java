624eb4fbe0576e1429d87242b3ac4b72b2899ba0
FemkeSpaans <55233583+FemkeSpaans@users.noreply.github.com>
2020-09-18 12:41:59 +0200

    Add files via upload

--- H2O.java
+++ H2O.java
@@ -9,6 +9,7 @@ where a water molecule can be seen depicted on the screen
  */
 public class H2O extends JFrame implements ActionListener {
     private JPanel panel;
+    public JTextField text1, text2;
 
 /* main in which the base of the gui is created
 using a frame
@@ -27,15 +28,16 @@ using a frame
         Container window = getContentPane();
         window.setLayout(new FlowLayout());
 
-        JTextField text1 = new JTextField("X axis");
-        JTextField text2 = new JTextField("Y axis");
-        window.add(text1);
-        window.add(text2);
+        JTextField text1 = new JTextField("X"); // create textfield X
+        JLabel label1 = new JLabel("Enter X value"); // create label X
+        window.add(text1); // add textfield X to window
+        window.add(label1); // add label X to window
 
-//        JLabel label1 = new JLabel("Enter X value"); // create label X
-//        JLabel label2 = new JLabel("Enter Y value"); // create label Y
-//        window.add(label1); // add label X to window
-//        window.add(label2); // add label Y to window
+
+        JTextField text2 = new JTextField("Y"); // create textfield Y
+        JLabel label2 = new JLabel("Enter Y value"); // create label Y
+        window.add(text2); // add textfield Y to window
+        window.add(label2); // add label Y to window
 
         JButton button = new JButton("Submit"); // create button
         window.add(button); // add button to window
@@ -45,9 +47,7 @@ using a frame
         panel.setBackground(Color.LIGHT_GRAY); // give panel a colour
         window.add(panel); // add panel to window
 
-        button.addActionListener(e ->{
-
-        }); // tell code to listen to button
+        button.addActionListener(this); // tell code to listen to button
 
     }
 
@@ -58,19 +58,28 @@ using a frame
      */
     public void actionPerformed(ActionEvent event) {
         Graphics paper = panel.getGraphics();
-        super.paintComponents(paper);
 
+        int x = Integer.parseInt(text1.getText());
+        int y = Integer.parseInt(text2.getText());
+
+        // carbon
         paper.setColor(Color.RED); // makes oval red
         paper.drawOval(40, 40, 60, 60); // draws an oval in these dimensions
-        paper.fillOval(40,40,60,60);
+        paper.fillOval(x + 62, y + 62, 76, 76);
 
         paper.setColor(Color.BLACK); // makes line black
-        paper.drawLine(10, 10, 40, 10);
+        paper.drawLine(x + 25, y + 25, x + 100, y + 100);
 
+        paper.drawLine(x + 100, y + 100, x + 25, y + 175);
+
+        //hydrogen 1
         paper.setColor(Color.WHITE); // makes oval white
-        paper.drawOval(20,20,30,30);
-        paper.fillOval(20,20,30,30);
+        paper.drawOval(20,20,50,50);
+        paper.fillOval(x,y,50,50);
 
+        // hydrogen 2
+        paper.drawOval(20,20,50,50);
+        paper.fillOval(x,y,50,50);
 
     }
 
