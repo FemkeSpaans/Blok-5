c2d2a33da5bde2f4fa45556db543c8ee5f3e2878
FemkeSpaans <55233583+FemkeSpaans@users.noreply.github.com>
2020-10-09 14:03:57 +0200

    Add files via upload

--- OneTwoThree.java
+++ OneTwoThree.java
@@ -13,43 +13,41 @@ public class OneTwoThree extends JFrame {
         //print();
     }
 
-    public static void frame(){
+    public static void frame() {
         JFrame frame = new JFrame(); // create new object
         frame.getContentPane().setLayout(null);
         frame.setTitle("Translator"); // give title to frame
-        frame.setSize(400,400);
+        frame.setSize(400, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // close frame when closing
 
 
         JTextField textfield = new JTextField();
-        textfield.setBounds(20,20, 360,50);
+        textfield.setBounds(20, 20, 360, 50);
         frame.add(textfield);
 
         JButton button = new JButton();
         button.setText("Submit");
-        button.setBounds(140,90, 100,50);
+        button.setBounds(140, 90, 100, 50);
         frame.add(button);
-        button.addActionListener(e ->{
+        button.addActionListener(e -> {
             String text = textfield.getText();
             text.equalsIgnoreCase(text);
 
             JLabel label = new JLabel();
             label.setBounds(20, 180, 360, 100);
             frame.add(label);
-
-            for(String i = text;;){
+            String output = "";
+            for (int i = 0; i < text.length(); i++) {
+                char var = text.charAt(i);
+                Character.toString(var);
                 try {
-                    Translator.one2three(i);
+                    output += Translator.one2three(Character.toString(var));
                 } catch (NotAnAA notAnAA) {
                     notAnAA.printStackTrace();
                 }
-                label.setText(Translator.THREE[Integer.parseInt(i)]);
-                break;
             }
+            label.setText(output);
         });
         frame.setVisible(true); // makes frame visible
     }
-
-
-    }
-
+}
