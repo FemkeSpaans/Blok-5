033fcfa84a5b5087fb3bb94ac1f22dc83ba21ded
FemkeSpaans <55233583+FemkeSpaans@users.noreply.github.com>
2020-10-14 18:30:45 +0200

    Add files via upload

--- GUIPolarity.java
+++ GUIPolarity.java
@@ -1,15 +1,27 @@
 package Afvink6one.java;
 
-import Afvink6.FileReader;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
+//import java.util.Arrays;
+//import java.util.List;
+
+/*
+get path from browse button en set text of the textfield,
+with analyse button get the text from the textfield and open that file
+ */
 
 public class GUIPolarity extends JFrame implements ActionListener {
+    JButton browse_button, analayse_button;
+    JLabel label = new JLabel();
+    JTextField textfield = new JTextField();
+    JTextArea textarea = new JTextArea();
+    JPanel panel = new JPanel();
+    BufferedReader inFile;
+    JFileChooser fileChooser;
+
     public static void main(String[] args) {
         GUIPolarity frame = new GUIPolarity(); //create a new frame
         frame.setTitle("Polarity display");
@@ -20,37 +32,30 @@ public class GUIPolarity extends JFrame implements ActionListener {
         frame.setVisible(true); // makes frame visible
 
     }
-    public void createGUI(){
 
+    public void createGUI() {
         Container window = getContentPane();
 
-        JLabel label = new JLabel();
         label.setBounds(20, 20, 40, 20);
         label.setText("File");
         window.add(label);
 
-        JTextField textfield = new JTextField();
         textfield.setBounds(60, 20, 320, 20);
         window.add(textfield);
 
-
-        JButton browse_button = new JButton();
         browse_button.setBounds(420, 20, 100, 20);
         browse_button.setText("Browse");
         browse_button.setBackground(Color.GRAY);
         browse_button.setForeground(Color.BLACK);
         window.add(browse_button);
 
-        JTextArea textarea = new JTextArea();
-        textarea.setBounds(20, 60,740, 400);
+        textarea.setBounds(20, 60, 740, 400);
         window.add(textarea);
 
-        JPanel panel = new JPanel();
         panel.setBounds(20, 500, 740, 240);
         panel.setBackground(Color.WHITE);
         window.add(panel);
 
-        JButton analayse_button = new JButton();
         analayse_button.setBounds(540, 20, 100, 20);
         analayse_button.setText("Analyse");
         analayse_button.setBackground(Color.GRAY);
@@ -62,26 +67,73 @@ public class GUIPolarity extends JFrame implements ActionListener {
         analayse_button.addActionListener(this);
     }
 
+    public void readFile() {
+
+        try {
+            inFile = new BufferedReader(new FileReader(textfield.getText()));
+            textarea.setText("");
+            String line;
+            while ((line = inFile.readLine()) != null) {
+                textarea.append(line + "\n");
+            }
+            inFile.close();
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null,
+                    "File Error: " + e.toString());
+        }
+    }
+
+
+//    public static void calculate(BufferedReader reader) throws IOException {
+//        String line;
+//
+//        float total_amino_acids = 0f;
+//        float polar_amino_acids = 0f;
+//        float nonpolar_amino_acids = 0f;
+//
+//        while ((line = reader.readLine()) != null) { // while there are lines to go
+//            if (line.startsWith(">")) {
+//            } else {
+//                total_amino_acids += line.length(); // add 1 to this int
+//                java.util.List<String> list_polar = Arrays.asList(AminoAcids.Polar);
+//                List<String> list_non_polar = Arrays.asList(AminoAcids.Nonpolar);
+//                for (int i = 0; i < line.length(); i++) {
+//                    char var = line.charAt(i);
+//                    String var1 = Character.toString(var);
+//                    if (list_polar.contains(var1)) {
+//                        polar_amino_acids++;
+//                    }
+//                    if (list_non_polar.contains(var1)) {
+//                        nonpolar_amino_acids++;
+//                    }
+//                }
+//            }
+//        }
+//        float percentage_polar = polar_amino_acids / total_amino_acids * 100;
+//        float percentage_non_polar = nonpolar_amino_acids / total_amino_acids * 100;
+//        //System.out.println(Math.round(percentage_polar));
+//        //System.out.println(Math.round(percentage_non_polar));
+//    }
+
     @Override
     public void actionPerformed(ActionEvent evt) {
-        if (evt.getSource()==browse_button)
-        {
-            try {
-                BufferedReader reader1 = FileReader.file();
-                //textfield.setText(); display filename when file is chosen
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
+        File selectedFile;
+        int reply;
+        if (evt.getSource() == browse_button) {
+            fileChooser = new JFileChooser();
+            reply = fileChooser.showSaveDialog(this);
+            if (reply == JFileChooser.APPROVE_OPTION) {
+                selectedFile = fileChooser.getSelectedFile();
+                textfield.setText(selectedFile.getAbsolutePath());
             }
-
         }
-        else if (evt.getSource()==analayse_button)
-        {
-            try {
-                BufferedReader reader1 = FileReader.file();
-                FileReader.calculate(reader1);
-                //textarea.setText(); // display contents of the file
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
+        if (evt.getSource() == analayse_button) {
+            fileChooser = new JFileChooser();
+            reply = fileChooser.showOpenDialog(this);
+            if (reply == JFileChooser.APPROVE_OPTION) {
+                selectedFile = fileChooser.getSelectedFile();
+                textfield.setText(selectedFile.getAbsolutePath());
+                readFile();
             }
         }
 
