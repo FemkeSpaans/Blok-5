12dbbf0163c7fb3bb51ddc8e32be0de7215799d7
FemkeSpaans <55233583+FemkeSpaans@users.noreply.github.com>
2020-10-29 15:33:21 +0100

    Add files via upload

--- Sequence.java
+++ Sequence.java
@@ -2,15 +2,19 @@ package Afvink7;
 
 public abstract class Sequence {
     public String sequence;
+    public int value;
 
     public void Sequence(String sequence) {
         this.sequence = sequence;
     }
-public enum Colors {
-    RED,
-    BLUE,
-    GREY,
-    YELLOW
+public enum Color {
+    RED(0),
+    BLUE(1),
+    GREY(2),
+    YELLOW(3);
+
+    Color(int i) {
+    }
 }
 
     public void setSequence(String sequence) {
@@ -23,5 +27,9 @@ public enum Colors {
 
     public int getLength() {
         return this.sequence.length();
+    }
+    public int getColor(int integer){
+        this.value = integer;
+        return this.value;
     }
 }
